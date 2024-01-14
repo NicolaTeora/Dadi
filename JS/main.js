b4016c08f471e47e44b1cd33bf222982fdf11a1e
NicolaTeora <nikxodia@gmail.com>
2024-01-14 20:18:25 +0100

    feat : view point on page

--- JS/main.js
+++ JS/main.js
@@ -3,15 +3,26 @@ let userDice = Math.floor((Math.random() * 6) + 1);
 let computerDice = Math.floor((Math.random() * 6) + 1);
 let result = '';
 
+window.onload = function() {
+    document.getElementById('userResult').innerText = userDice;
+    document.getElementById('computerResult').innerText = computerDice;
+    document.getElementById('viewPoint').innerText = result;
+}
 console.log(userDice);
 console.log(computerDice);
 
 //faccio i confronti tra i risultati
 if (userDice < computerDice) {
     result = 'Computer vince!';
+
 } else if (computerDice < userDice) {
-    result = 'Tu hai vinto!';
+    result = 'Giocatore ha vinto!';
+
 } else {
     result = 'Pareggio!'
+
 };
-    console.log(result);
+
+document.getElementById('viewPoint').innerText = result;
+
+console.log(result);
